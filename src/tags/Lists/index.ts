59d8b8f0f479c37efa985a92252f125e0afa2dc1
Rat <rat.emai@gmail.com>
2020-03-22 18:43:31 -0500

    fix misspell

--- src/tags/Lists/index.ts
+++ src/tags/Lists/index.ts
@@ -2,8 +2,8 @@ import {list as listConvertion} from './listConvertion'
 
 export const list=(text:string)=>{
 	try{
-		var ttext=listConvertion(text,'ul','[-\\*]\\s(.*)')
-		ttext=listConvertion(text,'ol','\\d[\\.\\)]\\s(.*)')
+		text=listConvertion(text,'ul','[-\\*]\\s(.*)')
+		text=listConvertion(text,'ol','\\d[\\.\\)]\\s(.*)')
 
 		return text
 	}catch{
