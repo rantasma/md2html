63afff9ba913c085b3130833fc0652717dcad1b8
Rat <rat.emai@gmail.com>
2020-03-24 17:59:20 -0500

    add navigation and update methods

--- src/md2html/index.ts
+++ src/md2html/index.ts
@@ -5,18 +5,23 @@ import {IgnoreRanges as Ranges} from './../ignoreRanges/index'
 
 import {DefaultFilters} from './../default-filter/index'
 import {defaultIgnoreMethods} from './../default-ignoreRanges/index'
+import {Variables} from './../variables/index'
 // import {linkVariables} from './misselaneous/linkVariables'
 
 
 export class Md2Html {
-	private filters:Array<Filter>
-	private variables:Array<string>
-	private ranges:Ranges
 	private htmlText:string;
+	private filters:Array<Filter>
+
+	private variables:Variables
+	private ranges:Ranges
 
 	constructor(public text:string) {
+
 		this.ranges=new Ranges(text)
 		this.ranges.addMulti(defaultIgnoreMethods)
+
+		this.variables=new Variables()
 
 		this.filters=[...DefaultFilters]
 	}
@@ -50,58 +55,76 @@ export class Md2Html {
 	}
 
 	getHtml(){
+
 		return this.htmlText
 	}
 
 	addFilter(filter:Filter):void{
+
 		filter.priority=filter.priority || this.filters.length
+
+		if(!this.validateNewPriority(filter.priority)){
+			return false
+		}
+
+		this.updateFilterPriority(filter.priority)
 		this.filters.push(filter)
 	}
 
 	removeFilter(filterName:string):void{
+
 		this.filters=this.filters.filter(filter=>{
+
 			return filterName != filter.name
 		})
 	}
+
+	changePriority(filterName:string,newPriority:number){
+
+		if(!this.validateNewPriority(newPriority)){
+			return false
+		}
+
+		var targetFilter=this.filters[filterName]
+
+		if (targetFilter.priority > newPriority) {
+
+			this.updateFilterPriority(newPriority,targetFilter.priority)
+
+		}else{
+
+			this.updateFilterPriority(targetFilter.priority,newPriority,false)
+		}
+
+		this.filters[filterName].priority=newPriority
+	}
+
+	updateFilterPriority(from:number=0,to:number=this.filters.length,direction:boolean=true){
+
+		for (let i = from; i <= to; i++) {
+
+			const filter = this.filters[i];
+
+			if (direction) {
+
+				filter.priority++
+			}else{
+
+				filter.priority--
+			}
+		}
+	}
+
+	validateNewPriority(newPriority:number){
+
+		if (newPriority < 0 || newPriority > this.filters.length) {
+
+			console.error(`change priority ${newPriority} is not valid. is has to be a number between 0 - ${this.filters.length}`)
+			return false
+
+		}else{
+
+			return true
+		}
+	}
 }
-
-
-
-// class Md2html {
-// 	static Tag=Tag
-//
-// 	constructor() {}
-//
-// 	static getHtml(text:string){
-// 		var data=linkVariables(text)
-//
-// 		var t=data.text;
-// 		var variables=data.variables
-//
-// 		Md2html.Tag.forEach((func,index)=>{
-// 			try{
-// 				t=func(t,variables)
-// 			}catch(e){
-// 				console.error('Md2html error: somethis is not working between filter '+ (isNaN(index-1)?0:index-1) +' and '+index);
-// 				if (this.Tag.length > 11) {
-// 					console.error('be shure you are returning a multiline text in your custome filter')
-// 				}
-// 		})
-//
-// 		return t
-// 	}
-// 	static add(filter:Function,priority:number){
-// 		if (!priority) {
-// 			this.Tag.push(filter)
-// 		}else{
-//
-// 			var copy=[...this.Tag].slice(0,priority-1)
-// 			copy.push(filter)
-// 			this.Tag=[
-// 				...copy,
-// 				...this.Tag.slice(priority-1)
-// 			]
-// 		}
-//
-// 	}
-// }
